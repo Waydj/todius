fc47530d9e3ec04d277c36a5f2f774add289d9fa
waydj <farhminait@gmail.com>
2023-11-21 20:07:52 +0200

    add OrganizationLayout, redirect to orgId

---
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -0,0 +1,12 @@
+const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="pt-20 md:pt-24 max-w-6xl 2xl:max-w-screen-2xl mx-auto">
+      <div className="flex gap-x-7">
+        <div className="w-64 shrink-0 hidden md:block">{/* navbar */}</div>
+        {children}
+      </div>
+    </main>
+  );
+};
+
+export default OrganizationLayout;
