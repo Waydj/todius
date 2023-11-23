03553eca04e4df727b1864ead7bb1d8020f5fd51
Waydj <farhminait@gmail.com>
2023-11-23 12:16:06 +0200

    add sidebar,accordion,separator,skeleton

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -6,7 +6,9 @@ import { Plus } from "lucide-react";
 const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
-      {/* TODO: mobile */}
+      
+      
+      
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,8 +1,12 @@
+import Sidebar from "../_components/sidebar";
+
 const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="pt-20 md:pt-24 max-w-6xl 2xl:max-w-screen-2xl mx-auto">
       <div className="flex gap-x-7">
-        <div className="w-64 shrink-0 hidden md:block">{/* navbar */}</div>
+        <div className="w-64 shrink-0 hidden md:block">
+          <Sidebar />
+        </div>
         {children}
       </div>
     </main>
